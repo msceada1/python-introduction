0dafc2a36dbd74d9217336890a314832a3083599
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-18 10:55:47 +0200

    ejercicio 3

---
+++ boletin_2/ejercicio_3.py
@@ -0,0 +1,10 @@
+# Crea una función llamada calcular_perimetro_cuadrado(lado) que reciba la medida del lado
+# de un cuadrado.
+# ● Importante: La función NO debe imprimir nada, debe devolver (return) el resultado.
+# ● Captura el resultado en una variable y muéstralo fuera de la función.
+
+def calcular_perimetro_cuadrado(lado):
+    return lado * 4
+
+if __name__ == '__main__':
+    print(calcular_perimetro_cuadrado(6))
