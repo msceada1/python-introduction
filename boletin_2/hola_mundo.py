a4d91232f9f7bbd23bcb5bf611c89146891cac13
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-18 10:55:35 +0200

    ejercicio 1

---
+++ boletin_2/hola_mundo.py
@@ -0,0 +1,10 @@
+# Crea una función llamada saludar() que no reciba parámetros. Al llamarla, debe imprimir el
+# mensaje: "¡Hola, mundo de Python!".
+
+
+def saludar():
+    print("Hola Mundo desde Python")
+
+
+if __name__ == '__main__':
+    saludar()
