b160419f9ba533a918771491cb2c3bbe88750109
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-18 10:55:53 +0200

    ejercicio 4

---
+++ boletin_2/ejercicio_4.py
@@ -0,0 +1,9 @@
+# Define una función llamada calcular_area_rectangulo(base, altura) que reciba dos parámetros
+# y devuelva el área.
+# ● Fórmula: base * altura.
+
+def calcular_area_rectangulo(base, altura):
+   return base * altura
+
+if __name__ == '__main__':
+    print(calcular_area_rectangulo(4, 3))
