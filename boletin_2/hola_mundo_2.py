d252990918f30f3a6fcbf43417eb0096e2b17d21
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-18 10:55:41 +0200

    ejercicio 2

---
+++ boletin_2/hola_mundo_2.py
@@ -0,0 +1,10 @@
+# Crea una función llamada saludar_usuario(nombre) que reciba un parámetro nombre.
+# ● Si llamas a saludar_usuario("Ana"), debe imprimir: "¡Hola, Ana!".
+# ● Prueba: Llama a la función dos veces con nombres distintos.
+
+def saludar(nombre):
+    print("hola", nombre)
+
+if __name__ == '__main__':
+    saludar("Manuel")
+    saludar("Manolo")
