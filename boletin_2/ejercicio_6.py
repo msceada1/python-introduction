00e66a9c76313e0fac1bfcc9eb040decbb2c20fe
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-05-18 10:56:02 +0200

    ejercicio 6

---
+++ boletin_2/ejercicio_6.py
@@ -0,0 +1,8 @@
+# Python incluye un módulo llamado math con funciones matemáticas avanzadas.
+# 1. Importa el módulo: import math.
+# 2. Usa math.sqrt(numero) para imprimir la raíz cuadrada de 144.
+# 3. Imprime el valor de math.pi.
+from math import pi
+
+if __name__ == '__main__':
+    print(pi)
